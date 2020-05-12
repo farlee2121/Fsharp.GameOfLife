888a21b5c876d801422cf520ce6322932f47e146
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2020-05-12 13:04:24 -0500

    Refactor rules to handle the neighborhood a little more cleanly

--- GameOfLife.Core/LifeRules.fs
+++ GameOfLife.Core/LifeRules.fs
@@ -5,8 +5,9 @@
 type CellState = Alive | Dead
 type Colony = CellState[,]
 
-module Array2D = 
+module Colony = 
   let toArray (array2D: 'T [,]) = array2D |> Seq.cast<'T> |> Seq.toArray
+  let mapi = Array2D.mapi 
 
 // Rejected coordinate generation comb 2 [-1;0;1;-1;0;1]
 //let rec comb n l = 
@@ -15,7 +16,7 @@ module Array2D =
 //    | _, [] -> []
 //    | k, (x::xs) -> List.map ((@) [x]) (comb (k-1) xs) @ comb k xs
 
-let isInBounds i j colony = 
+let isInBounds colony i j  = 
     let isValidVerticalIndex = 0 <= i && i < Array2D.length1 colony
     let isValidHorizontalIndex =  0 <= j && j < Array2D.length2 colony
     isValidVerticalIndex && isValidHorizontalIndex
@@ -23,34 +24,34 @@ let isInBounds i j colony =
 let isCellAlive i j (colony: Colony) = 
     colony.[i,j] = Alive
 
-let countNeighbors i j (colony: Colony) = 
-    Array2D.init 3 3 (fun i' j' ->
-        match (i + (i'-1), j+(j'-1)) with
-        | (i'',j'') when i'' = i && j'' = j -> false
-        | (iFinal, jFinal) -> (isInBounds iFinal jFinal colony) && (isCellAlive iFinal jFinal colony) // this feels wrong how could I track this a more compositional approach
+let getNeighbors i j (colony: Colony) = 
+    let isInColony (i',j') = isInBounds colony i' j'
+    let neighborIndexes = (Array2D.init 3 3 (fun i' j' -> (i'-1, j'-1)) 
+        |> Colony.toArray 
+        |> Array.filter (fun t ->
+            match t with
+            | (0,0) -> false
+            | _ -> true
         )
-    |> Array2D.toArray 
-    |> Array.filter ((=) true) |> Array.length
-
-    //let neighborIndexes = (Array2D.init 3 3 (fun i' j' -> (i'-1, j'-1)) 
-    //    |> Array2D.toArray 
-    //    |> Array.filter (fun t -> match t with
-    //        | (0,0) -> false
-    //        | _ -> true)
-    //) // would use mapi from here
+        |> Array.map (fun (iOffset,jOffset) -> (i+iOffset, j+jOffset))
+        |> Array.filter isInColony
+    )
+    neighborIndexes |> Array.map (fun (i',j') -> colony.[i', j'])
 
     
     
 
-let stepCell i j (colony : Colony) = 
-    match (countNeighbors i j colony) with
+let stepCell (currentState: CellState) (neighbors : CellState seq) = 
+    let livingNeighbors = neighbors |> Seq.filter ((=) Alive) |> Seq.length
+    match (livingNeighbors) with
     | n when n < 2 -> Dead
-    | 2 -> colony.[i,j]
+    | 2 -> currentState
     | 3 -> Alive
     | n when n > 3 -> Dead
     | n -> failwithf "Impossible number of cell neighbors %i" n
 
 let timeStep (colony: Colony) : Colony =
     // question, should I pass neighbors here instead of the whole colony?
-    Array2D.mapi (fun i j value -> stepCell i j colony) colony
-
+    colony |> Colony.mapi (fun i j cellState -> 
+        stepCell cellState (getNeighbors i j colony)
+        ) 
